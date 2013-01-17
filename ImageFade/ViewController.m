978956a2ba1b6319dc0301ff03b4d3c39b768bca
Alexis Gallagher <alexis@alexisgallagher.com>
2013-01-17 17:42:51 +0000

    initial commit

--- ImageFade/ViewController.m
+++ ImageFade/ViewController.m
@@ -9,14 +9,19 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (strong,nonatomic) UIImage * imageOne;
+@property (strong,nonatomic) UIImage * imageTwo;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
+  self.imageOne = [UIImage imageNamed:@"mandel200x200.jpg"];
+  self.imageTwo = [UIImage imageNamed:@"mandel_images.png"];
+  
+  self.imageView.image = self.imageOne;
+  [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -26,4 +31,16 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)handleButtonTouch:(id)sender {
+  
+  UIImage * toImage = ([self.imageView.image isEqual:self.imageOne]) ? self.imageTwo : self.imageOne;
+  
+  [UIView transitionWithView:self.imageView
+                    duration:5.0f
+                     options:UIViewAnimationOptionTransitionCrossDissolve
+                  animations:^{
+                    self.imageView.image = toImage;
+                  }
+                  completion:NULL];
+}
 @end
